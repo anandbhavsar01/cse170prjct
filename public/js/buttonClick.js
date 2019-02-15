23cdf419a620d7bbe4569d5a5fab357dc50c7702
cdouaihyucsd <cdouaihy@ucsd.edu>
2019-02-14 16:46:37 -0800

    Entry change

--- public/js/buttonClick.js
+++ public/js/buttonClick.js
@@ -12,9 +12,24 @@ function initializePage() {
 	$('.selectCategory').click(function(e) {
 		if($(this).attr("class") == "selectCategory"){
 			console.log("Button is selected " + $(this).attr("value"));
-			$('.entry').append;
-			console.log("Current entry is: " + $('.entry').val());
 		}
 		$(this).toggleClass("selected");
+		
+		
+	});
+	$('.submitButton').click(function(e) {
+		var elements = document.querySelectorAll("input[type=button]");   
+
+		var categories = " ";
+		for(var i = 0, len = elements.length; i < len; i++) {   
+  			console.log("Button: " +  elements[i].value);
+  			console.log(elements[i].className);
+  			if(elements[i].className == "selectCategory selected"){
+  				categories += elements[i].value + " ";
+  			}
+		}
+ 
+		$('.entry').val($('.entry').val() + categories);
+		console.log("Current entry is: " + $('.entry').val());
 	});
 }
