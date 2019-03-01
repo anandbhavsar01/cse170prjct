6e082049a6bbfacbfee3a320e8e669bf2b801083
Anand Bhavsar <abhavsar@ucsd.edu>
2019-02-28 16:32:26 -0800

    Routing for edit

--- routes/edit.js
+++ routes/edit.js
@@ -1,3 +1,19 @@
+
+var data = require('../categories.json');
+
 exports.view = function(req, res){
-    res.render('editBucket');
+    var check = req.query;
+    var editData = { name: 'Error', include: [], exclude: ['']};
+    for(var i = 0; i < data["categories"].length; i++){
+        console.log(data["categories"][i]);
+        if(data["categories"][i].name == check.entry){
+            editData = data["categories"][i];
+        }
+    }
+    console.log(editData);
+    res.render('editBucket', editData);
 };
+
+exports.editView = function(req, res){
+    res.render('edit', data);
+}
