9acbed57a44c6a769adfe916f410286ab99be58d
Anand Bhavsar <abhavsar@ucsd.edu>
2019-02-28 21:02:32 -0800

    A/B testing

--- routes/index.js
+++ routes/index.js
@@ -7,3 +7,7 @@ var data = require('../categories.json');
 exports.view = function(req, res){
   res.render('index', data);
 };
+
+exports.viewAlt = function(req, res){
+  res.render('index_B', data);
+}
