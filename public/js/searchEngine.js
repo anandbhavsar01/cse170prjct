4f7867c15116567ca609b99dfdf8208e59611a21
Anand Bhavsar <abhavsar@ucsd.edu>
2019-03-07 15:44:45 -0800

    Fixed search engine issue

--- public/js/searchEngine.js
+++ public/js/searchEngine.js
@@ -19,16 +19,36 @@ exports.makeQuery = function(req, response){
     var searchTerm = " +" + req.query.entry;
     var categoryName = req.query.filter;
     for(var i = 0; i< categories["categories"].length; i++){
-        if(categoryName.localeCompare(categories["categories"][i].name) == 0){
-            console.log(categories["categories"][i].include);
-            console.log(categories["categories"][i].exclude);
-            for(var j = 0; j < categories["categories"][i].include.length; j++){
-                searchTerm = searchTerm + "+" + categories["categories"][i].include[j].term + " ";
+        if(categoryName != undefined){
+            if(categoryName.localeCompare(categories["categories"][i].name) == 0){
+                for(var j = 0; j < categories["categories"][i].include.length; j++){
+                    searchTerm = searchTerm + "+" + categories["categories"][i].include[j].term + " ";
+                }
+                for(var j = 0; j < categories["categories"][i].exclude.length; j++){
+                    searchTerm = searchTerm + "-" + categories["categories"][i].exclude[j].term + " ";
+                }
+                info = {
+                    url: 'https://api.cognitive.microsoft.com/bingcustomsearch/v7.0/search?' + 
+                        'q=' + searchTerm + "&" +
+                        'customconfig=' + customConfigId,
+                    headers: {
+                        'Ocp-Apim-Subscription-Key' : subscriptionKey
+                    }
+                }
+                var outResponse = response;
+                request(info, function(error, response, body){
+                    var searchResponse = JSON.parse(body);
+                    data.pages = [];
+                    if( searchResponse.webPages){
+                        for(var i = 0; i < searchResponse.webPages.value.length; ++i){
+                            var webPage = searchResponse.webPages.value[i];
+                            data.pages.push(webPage);
+                        }
+                        outResponse.render('results', data);
+                    }
+                });
             }
-            for(var j = 0; j < categories["categories"][i].exclude.length; j++){
-                searchTerm = searchTerm + "-" + categories["categories"][i].exclude[j].term + " ";
-            }
-            console.log(searchTerm);
+        } else {
             info = {
                 url: 'https://api.cognitive.microsoft.com/bingcustomsearch/v7.0/search?' + 
                     'q=' + searchTerm + "&" +
